239c00586bc436585b627dea8f7a63dca274cd7d
Richard Piacentini <richard.piacentini@nuxos.asia>
2022-11-25 23:34:11 +0700

    feat: if rank == 0 there is apparently no level

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,9 @@
 import { utils, BigNumberish } from "ethers";
 
 export function rankToEmber(rank: number, level: number): number {
-  return rank * 5 - (level - 1);
+  // Monks can have levels 1 to 5 if rank > 0
+  level = rank > 0 ? level - 1 : 1;
+  return rank * 5 - level;
 }
 
 export function toFixedGwei(num: BigNumberish, place: number = 2): string {
